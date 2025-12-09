a6eeee57e0c782ac4a284b4ef846c249a6246f28
유해린 <dbgofls060816@gmail.com>
2025-12-09 11:29:48 +0900

    Update title to Haerin Portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,7 @@ const Hero = () => {
         <section id="about" className="hero">
             <div className="container hero__container">
                 <h1 className="hero__title">
-                    Creative <span className="highlight-pill">Web</span><br />
-                    Design <span className="circle-icon">&</span> Art
+                    Haerin <span className="highlight-pill">Portfolio</span>
                 </h1>
                 <p className="hero__subtitle">
                     Creating immersive digital experiences. Check out my works below.
